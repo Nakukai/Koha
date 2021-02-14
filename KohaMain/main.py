ffcd9be5b1165eeb6718ce0de7888dc8f3d8300d
Katsuniya <katsuniya@gmail.com>
2021-02-14 03:07:04 +0000

    Update main.py

--- KohaMain/main.py
+++ KohaMain/main.py
@@ -6,4 +6,12 @@ Token = ''
 client = commands.Bot(command_prefix=["K!", "k!"], case_insensitive=True)
 client.remove_command('help')
 
+# EVENTS THAT OCCUR ON STARTUP
+@client.event
+async def on_ready():
+    print("Mayu V 0.0.1 is live")
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
 client.run(Token)
