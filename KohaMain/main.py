a35681368c7efe95b36ede5844ff5b1d0f60928a
Nana Ntim <75762512+Katsuniya@users.noreply.github.com>
2021-02-14 01:37:30 +0000

    Update main.py

--- KohaMain/main.py
+++ KohaMain/main.py
@@ -2,5 +2,8 @@ import os
 import discord
 from discord.ext import commands
 
+Token = ''
 client = commands.Bot(command_prefix=["K!", "k!"], case_insensitive=True)
 client.remove_command('help')
+
+client.run(Token)
