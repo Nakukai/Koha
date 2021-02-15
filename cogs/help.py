21460524b40a0bec7da2217738b97777f75b5980
Katsuniya <katsuniya@gmail.com>
2021-02-15 19:58:35 +0000

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -6,5 +6,9 @@ class Help(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command()
+    async def help(self, ctx):
+        pass
+
 def setup(client):
     client.add_cog(Help(client))
