681e286997acd09240f2b76c0501dbbca855e84e
Katsuniya <katsuniya@gmail.com>
2021-02-15 23:51:08 +0000

    Update

---
+++ cogs/main.py
@@ -0,0 +1,9 @@
+'''
+MAIN FILE DIRECTORY FOR ALL ATTACK AND POINT GAINING RELATED MATERIAL
+
+Anything related to earning points in user's accounts will be done here
+
+It'll consist of different commands for attack, defense, survival etc...
+
+Commands will be fully described within each command codeline for elaboration.
+'''         
