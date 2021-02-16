0e1cc52b3db9ffda00a976215e9cb0b51ab3ecda
Katsuniya <katsuniya@gmail.com>
2021-02-16 22:13:37 +0000

    Update analysis.py

--- cogs/analysis.py
+++ cogs/analysis.py
@@ -6,12 +6,8 @@ class Analysis(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    @commands.group()
     async def compare(self, ctx, User: discord.User = None):
-        if User == None:
-            await ctx.send("Please ping someone after the command")
-        else:
-            pass
 
 def setup(client):
     client.add_cog(Analysis(client))
