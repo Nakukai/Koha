c108e31f499e8b70fd7a10d21895e9e2c6c6f72a
Nana Ntim <75762512+Katsuniya@users.noreply.github.com>
2021-02-16 12:42:47 +0100

    Update shop.py

--- cogs/shop.py
+++ cogs/shop.py
@@ -11,7 +11,20 @@ class Shop(commands.Cog):
 
     @commands.command()
     async def shop(self, ctx):
-        pass
+      Shop_Items = discord.Embed(
+          color=0xef476f,
+          title="Navya's Store",
+          description="All the best materials in Koha right here."
+      )
+      Shop_Items.add_field(
+          name="🔥 Stamifier",
+          value=2500
+      )
+      Shop_Items.add_field(
+          name="🍰 Transfer",
+          value=1000
+      )
+      await ctx.send(embed=Shop_Items)
 
 def setup(client):
     client.add_cog(Shop(client))
