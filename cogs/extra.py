6a78797b642e629d2b110e0b5e765b6f919b969a
Katsuniya <katsuniya@gmail.com>
2021-02-17 22:05:49 +0000

    Update extra.py

--- cogs/extra.py
+++ cogs/extra.py
@@ -5,5 +5,14 @@ class Extra(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command()
+    async def invite(self, ctx):
+        Invitation = discord.Embed(
+            color=0xFFB6C1, 
+            description="**Want me to join your amazing server?**\n"
+                        "[Tap here to invite me](%s)" % 
+                        ("https://discord.com/api/oauth2/authorize?client_id=783865415746977824&permissions=3533888&scope=bot"))
+        await ctx.send(embed=Invitation)
+
 def setup(client):
     client.add_cog(Extra(client))
